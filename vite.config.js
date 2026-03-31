1af07ecf46ebf22aee3d08de432abb58ce445538
Peter Wretmo <peter.wretmo@gmail.com>
2026-04-01 00:30:33 +0200

    feat: add code-splitting for vendor and creature chunks

--- vite.config.js
+++ vite.config.js
@@ -8,5 +8,29 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: true,
+    chunkSizeWarningLimit: 2500,
+    rolldownOptions: {
+      output: {
+        codeSplitting: {
+          groups: [
+            {
+              name: 'vendor-three',
+              test: /node_modules[\\/]three/,
+              priority: 20,
+            },
+            {
+              name: 'vendor-rapier',
+              test: /node_modules[\\/]@dimforge/,
+              priority: 15,
+            },
+            {
+              name: 'creatures',
+              test: /src[\\/]creatures[\\/](?!CreatureManager)/,
+              priority: 10,
+            },
+          ],
+        },
+      },
+    },
   },
 });
